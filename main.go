77047643f1fec36f62af991a8189fc948ff4e523
ckl <conrad.klaus@fokus.fraunhofer.de>
2024-04-20 19:48:38 +0200

    add 404 page

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -30,35 +31,49 @@ func init() {
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Handling request for path: %s", r.URL.Path)
 
-	// Remove the leading slash from path and check for empty path
 	path := r.URL.Path[1:]
 	if path == "" {
 		http.Error(w, "No path specified", http.StatusBadRequest)
 		return
 	}
 
-	// Query the database for the redirect URL based on the path
 	var redirectURL string
 	err := db.QueryRow("SELECT redirect_url FROM redirects WHERE path = $1", path).Scan(&redirectURL)
 	if err != nil {
-		switch {
-		case errors.Is(err, sql.ErrNoRows):
-			http.Error(w, "Not Found", http.StatusNotFound)
-		default:
+		if errors.Is(err, sql.ErrNoRows) {
+			serve404(w)
+		} else {
 			log.Printf("Failed to execute query: %v", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		}
 		return
 	}
 
-	// Redirect to the fetched URL
-	log.Printf("redirecting to: %v", redirectURL)
+	log.Printf("Redirecting to: %v", redirectURL)
 	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func serve404(w http.ResponseWriter) {
+	log.Printf("Serving 404 page")
+	page, err := os.ReadFile("static/404.html")
+	if err != nil {
+		log.Printf("Error loading 404 page: %v", err)
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	if _, err := w.Write(page); err != nil {
+		// Log the error if the response can't be written
+		log.Printf("Failed to write 404 page to response: %v", err)
+		// Return a standard HTTP 500 Internal Server Error as a fallback
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
 	http.HandleFunc("/", redirectHandler) // Set the handler for the root URL
-
 	fmt.Println("Server starting on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil)) // Start the server on port 8080
 }
